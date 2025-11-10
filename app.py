5560be070f97f1d853e0b80af87375b93bff6def
Joshua Luppes <lupjos@dunwoody.edu>
2025-11-10 09:57:56 -0600

    Combine safe and unsafe

--- app.py
+++ app.py
@@ -4,24 +4,51 @@ import bleach  # Library for advanced sanitization
 
 app = Flask(__name__)
 app.secret_key = 'devkey'
+
+# Store comments in memory (for demo purposes)
+comments = []
 comments_advanced = []
+
+
 def sanitize_comment(text):
     """Allow only safe HTML tags"""
     # Define which HTML tags are allowed
-    allowed_tags = ['p', 'br', 'strong', 'em', 'ul', 'ol', 'li', 'b', 'i']
-    
+    allowed_tags = ['p', 'br', 'strong', 'em',
+                    'ul', 'ol', 'li', 'b', 'i', 'h1']
+
     # Clean the input - remove any tags not in our allowed list
     clean_text = bleach.clean(text, tags=allowed_tags, strip=True)
     return clean_text
+
+
+@app.route('/comments')
+def show_comments():
+    """Display all comments"""
+    return render_template('comments.html', comments=comments)
+
+
+@app.route('/add', methods=['POST'])
+def add_comment():
+    """Add a new comment - THIS IS VULNERABLE!"""
+    comment_text = request.form.get('comment_text', '').strip()
+    if comment_text:
+        # UNSAFE: Store raw user input without any sanitization
+        comments.append(comment_text)
+        flash('Comment added successfully!')
+    else:
+        flash('Please enter a comment.')
+    return redirect(url_for('show_comments'))
+
 
 @app.route('/')
 def show_comments_advanced():
     return render_template('comments_advanced.html', comments=comments_advanced)
 
+
 @app.route('/add_advanced', methods=['POST'])
 def add_comment_advanced():
     comment_text = request.form.get('comment_text', '').strip()
-    
+
     if comment_text:
         # SAFE: Sanitize before storing
         clean_comment = sanitize_comment(comment_text)
@@ -30,6 +57,7 @@ def add_comment_advanced():
     else:
         flash('Please enter a comment.')
     return redirect(url_for('show_comments_advanced'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
